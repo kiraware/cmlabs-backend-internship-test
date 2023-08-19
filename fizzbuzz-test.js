2c7092ea756b9a7fbafdeaebe2d8ea3319a3c023
Kira <kira68784@gmail.com>
2023-08-19 12:05:01 +0800

    optimize

--- fizzbuzz-test.js
+++ fizzbuzz-test.js
@@ -1,5 +1,6 @@
 for (let i = 1; i <= 100; i++) {
-	if (i % 3 == 0 && i % 5 == 0) {
+	// i % 3 == 0 && i % 5 == 0 === i % 15 == 0
+	if (i % 15 == 0) {
 		console.log("FizzBuzz")
 	} else if (i % 3 == 0) {
 		console.log("Fizz")
